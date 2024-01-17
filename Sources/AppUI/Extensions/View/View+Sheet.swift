fb848e25c83952b58f9fb6f24557ce7c31213be3
Mark DiFranco <mpdifranco@icloud.com>
2024-01-17 11:28:11 -0500

    Updates to full screen sheet presentation on macOS

--- Sources/AppUI/Extensions/View/View+Sheet.swift
+++ Sources/AppUI/Extensions/View/View+Sheet.swift
@@ -9,17 +9,21 @@ import SwiftUI
 
 public extension View {
 
+    /// Presents the view as a sheet.
     func sheet(_ view: Binding<AnyView?>) -> some View {
         sheet(isPresented: Binding(isNotNil: view, defaultValue: nil)) {
             view.wrappedValue
         }
     }
 
-    #if os(iOS)
+    /// Presents the view as a full screen cover. On macOS, the view is presented as a sheet.
     func fullScreenCover(_ view: Binding<AnyView?>) -> some View {
+        #if os(macOS)
+        sheet(view)
+        #else
         fullScreenCover(isPresented: Binding(isNotNil: view, defaultValue: nil)) {
             view.wrappedValue
         }
+        #endif
     }
-    #endif
 }
